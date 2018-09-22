ea55f799eab0f5d0eab309871491e7c85175cc5d
Reme <31744912+PostalMist@users.noreply.github.com>
2018-09-21 20:54:04 -0400

    Finished Apple picker
    Basic full game has been implemented.

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+    public static float bottomY = -20f; // a
+   // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject); // b
+
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); // b
+                                                                            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed(); // c
+        }
+    }
+}
